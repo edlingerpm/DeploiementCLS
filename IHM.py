f369109ee78afc3af4f59f3996f1aceda2347546
edlingerpm <edlingerpm@yahoo.fr>
2020-06-21 19:56:49 +0200

    PMEdlinger
    Ajout d'une icone;
    Correction bug mineur;
    Interdiction changement de taille fenêtre.

--- IHM.py
+++ IHM.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Jun 20 18:37:06 2020
 
-@author: Pierre-Marie EDLINGER test
+@author: Pierre-Marie EDLINGER
 """
 
 from tkinter import *
@@ -29,7 +29,10 @@ def fermeTout():
     fenPrincipale.quit()
 
 def creerFichierModele():
-    Prog.CreerFichierTravail(FF.getOuEnregistrerFichier())
+    cheminFic = ""
+    cheminFic = FF.getOuEnregistrerFichier()
+    if cheminFic != "":
+        Prog.CreerFichierTravail(FF.getOuEnregistrerFichier())
 
 def creerFichiersCLS():
     if (varNomFicTravail.get() == ""):
@@ -47,61 +50,77 @@ def creerFichiersCLS():
                     choixRepFinal()
         else:
             choixFicDeTravail()
-    
+
+# def pointeur(event):
+#     print( "Clic détecté en X =" + str(event.x) +\
+#                             ", Y =" + str(event.y))
+        
 #========== Programme principal =============
 
 # Création du widget principal ("parent") :
 fenPrincipale = Tk()
 fenPrincipale.title("Création du fichier d'importation dans IBM Lotus")
-
+fenPrincipale.geometry("650x130")
+# fenPrincipale.bind("<Button-1>", pointeur) # Button-1 --> clic gauche; Button-2 --> clic molette; Button-3 --> clic droit
+fenPrincipale.resizable(False, False)
+try:
+    fenPrincipale.iconbitmap('icone.ico')
+except : None
 """
 #Pour changer l'icone'
 fenPrincipale.iconbitmap("questhead")
 """
+GAUCHE = 10
+LARGEURBOUTONS = 25
 
 # création des widgets "enfants" :
 # can1 = Canvas(fen1,bg='dark grey',height=250, width=250)
 # can1.pack(side=LEFT, padx =5, pady =5)
+#chaine.configure(text = "test") # pour changer le texte d'un label
 
-chaine2 = Label(fenPrincipale, text = 'Choix du répertoire où enregistrer les fichiers de travail :')
-chaine2.grid(row =0, column =0, sticky = W)
+# ---- Gestion de la zone "où enregistrer les fichiers"
+# ---- Label
+lblRep1 = Label(fenPrincipale, text = 'Choix du répertoire où enregistrer les fichiers de travail :')
+lblRep1.place(x=GAUCHE, y = 10)
 
-chaine = Label(fenPrincipale, text = 'Choix du fichier de travail :')
-chaine.grid(row =2, column =0, sticky = W)
-#chaine.configure(text = "test")
-
-# ---- Gestion de la zone de texte qui récupère le nom du répertoire où enregistrer les fichiers
+# ---- zone de texte 
 varNomRep = StringVar()
 entr2 = Entry(fenPrincipale, textvariable = varNomRep, width =100)
 varNomRep.set("")
-entr2.grid(row =1, column =0)
+entr2.place(x=GAUCHE, y= 30)
 
-# ---- Gestion de la zone de texte qui récupère le nom de fichier
+# ---- Bouton ------------------
+btnChoixRep = Button(fenPrincipale, text='...', width =2, command=choixRepFinal)
+btnChoixRep.place(x=620, y= 30)
+#*********************************************************
+
+# ---- Gestion de la zone "fichier de travail"
+# ---- Label
+lblFic2 = Label(fenPrincipale, text = 'Choix du fichier de travail :')
+lblFic2.place(x=GAUCHE, y=50)
+
+# ---- zone de texte
 varNomFicTravail = StringVar()
 entr1 = Entry(fenPrincipale, textvariable = varNomFicTravail, width =100) #    varTC.set("")
 varNomFicTravail.set("")
-entr1.grid(row =3, column =0)
+entr1.place(x=GAUCHE, y= 70)
 
+# ---- Bouton ------------------
+btnFicTravail = Button(fenPrincipale, text='...', width =2, command=choixFicDeTravail)
+btnFicTravail.place(x=620, y= 70)
+#*********************************************************
 
-# ---- Gestion des boutons ------------------
-btnChoixRep = Button(fenPrincipale, text='...', width =0, command=choixRepFinal)
-btnChoixRep.grid(row =1, column =1, sticky = W)
-btnChoixRep.width = 0
-
-btnFicTravail = Button(fenPrincipale, text='...', width =0, command=choixFicDeTravail)
-btnFicTravail.grid(row =3, column =1, sticky = W)
-
-btnCreerFicTravail = Button(fenPrincipale, text='Créer un modèle', width =0, command=creerFichierModele)
-btnCreerFicTravail.grid(row =DERNIERELIGNE, column =0, sticky =W)
+btnCreerFicTravail = Button(fenPrincipale, text='Créer un modèle', width =LARGEURBOUTONS, command=creerFichierModele)
+btnCreerFicTravail.place(x=GAUCHE, y= 100)
 
 btnCreerFichierCLS = Button(fenPrincipale, 
                             text='Créer le(s) fichier(s) à importer', 
-                            width =0, 
+                            width =LARGEURBOUTONS, 
                             command=creerFichiersCLS)
-btnCreerFichierCLS.grid(row =DERNIERELIGNE, column =1, sticky = E)
+btnCreerFichierCLS.place(x=200, y= 100)
 
-btnQuit = Button(fenPrincipale,text='Quitter', width =10, command=fermeTout)
-btnQuit.grid(row =DERNIERELIGNE, column =3, sticky =E)
+btnQuit = Button(fenPrincipale,text='Quitter', width =LARGEURBOUTONS, command=fermeTout)
+btnQuit.place(x=400, y= 100)
 
 # démarrage du réceptionnaire d'évènements (boucle principale) :
 fenPrincipale.mainloop()
